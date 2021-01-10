2a1efaee1b1b9c3f21c57843018793fe0b34c0c1
user <user@DESKTOP-A72HUFE>
2021-01-10 18:19:21 +0100

    customer

--- Gestion d'hotel/src/swing/Test.java
+++ Gestion d'hotel/src/swing/Test.java
@@ -1,57 +1,68 @@
 package swing;
 
-import java.sql.Connection;
- 
-import java.sql.Statement;
- 
-import java.sql.DriverManager;
- 
-import java.sql.ResultSet;
- 
-import java.sql.ResultSetMetaData;
- 
-import java.sql.SQLException;
-import javax.swing.DefaultListModel;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
- 
- 
- 
-public class Test extends JFrame{
- 
-private static javax.swing.JList<String> list;
- 
-public static void main(String args[]) throws ClassNotFoundException, SQLException
- 
-{
- 
-Connection con ;
- 
-Statement Statement ;
- 
-ResultSet resultSet ;
-ResultSetMetaData metaData ;
-int numberOfColumns = 0;
-DefaultListModel mod=new DefaultListModel();
- 
-try
- 
-{
- 
-Class.forName("com.mysql.jdbc.Driver");
- 
-con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3308/hotel?serverTimezone=UTC" ,"root","root");
- 
-//statement = connection.createStatement();
-Statement stmt=con.createStatement();  
-ResultSet rs=stmt.executeQuery("select * from admin");  
-while(rs.next())  
-System.out.println(rs.getString(1)+ " " + rs.getString(2) + " " + rs.getInt(3)+  " " + rs.getString(4) + " " + rs.getString(5));  
-con.close();  
- 
-System.out.println("Connection Established");
- 
- 
-}catch(ClassNotFoundException | SQLException e){
-    System.err.println(e);
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.RowFilter;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
+public class Test {
+     public static void main(String args[]) {
+       JFrame frame = new JFrame("Regexing JTable");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       Object rows[][] = { { "A", "About", 44.36 }, { "B", "Boy", 44.84 }, { "C", "Cat", 463.63 },
+           { "D", "Day", 27.14 }, { "E", "Eat", 44.57 }, { "F", "Fail", 23.15 },
+           { "G", "Good", 4.40 }, { "H", "Hot", 24.96 }, { "I", "Ivey", 5.45 },
+           { "J", "Jack", 49.54 }, { "K", "Kids", 280.00 } };
+       String columns[] = { "Symbol", "Name", "Price" };
+       TableModel model = new DefaultTableModel(rows, columns) {
+         public Class getColumnClass(int column) {
+           Class returnValue;
+           if ((column >= 0) && (column < getColumnCount())) {
+             returnValue = getValueAt(0, column).getClass();
+           } else {
+             returnValue = Object.class;
+           }
+           return returnValue;
+         }
+       };
+
+       final JTable table = new JTable(model);
+       final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
+       table.setRowSorter(sorter);
+       JScrollPane pane = new JScrollPane(table);
+       frame.add(pane, BorderLayout.CENTER);
+
+       JPanel panel = new JPanel(new BorderLayout());
+       JLabel label = new JLabel("Filter");
+       panel.add(label, BorderLayout.WEST);
+       final JTextField filterText = new JTextField("A");
+       panel.add(filterText, BorderLayout.CENTER);
+       frame.add(panel, BorderLayout.NORTH);
+       JButton button = new JButton("Filter");
+       button.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+           String text = filterText.getText();
+           if (text.length() == 0) {
+             sorter.setRowFilter(null);
+           } else {
+             sorter.setRowFilter(RowFilter.regexFilter(text));
+           }
+         }
+       });
+       frame.add(button, BorderLayout.SOUTH);
+       frame.setSize(300, 250);
+       frame.setVisible(true);
+     }
+
 }
-}}
